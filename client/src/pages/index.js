aa9043c132d85d679180f56d60cca4a20aa1b34b
Daniel Thomas <danjes002@gmail.com>
2023-04-26 14:00:02 +0530

    refactor: re-structure file directories

---
+++ client/src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import { Box, Center } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Center height="100vh">
+      <Box>Hey, you</Box>
+    </Center>
+  );
+}
