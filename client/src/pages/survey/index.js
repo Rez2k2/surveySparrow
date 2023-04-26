bf4bf37102ac06cecbb765f9859382602e7a64eb
rez2k2 <rameeznew2002@gmail.com>
2023-04-27 00:07:15 +0530

    talkBack 1.1

--- client/src/pages/survey/index.js
+++ client/src/pages/survey/index.js
@@ -1,14 +1,27 @@
 import { Button } from "@chakra-ui/react";
 
 export default function Survey() {
-  const speechMe = () => {
-    var msg = "Danny you lying piece of shit!";
-    const utterance = new SpeechSynthesisUtterance(msg);
+  var msg = "Danny you lying piece of shit!";
+  const speechMe = (msg1) => {
+    const utterance = new SpeechSynthesisUtterance(msg1);
     speechSynthesis.speak(utterance);
+  };
+  var ques = "This is the first question , and its read aloud";
+  const handleClick = (e) => {
+    speechMe(e.currentTarget.getAttribute("data"));
   };
   return (
     <div>
-      <Button onClick={speechMe}>Click me</Button>
+      <form>
+        <p>{ques}</p>
+        <input type="text" placeholder="answer here" />
+        <Button data={ques} onClick={handleClick}>
+          Read
+        </Button>
+      </form>
+      <Button data={msg} onClick={handleClick}>
+        Danny sound
+      </Button>
     </div>
   );
 }
