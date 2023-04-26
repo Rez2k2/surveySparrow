0205ae53eb4023d3a1a87399f76e23675bd224ea
rez2k2 <rameeznew2002@gmail.com>
2023-04-26 23:46:41 +0530

    talk_back

--- client/src/pages/survey/index.js
+++ client/src/pages/survey/index.js
@@ -1,3 +1,14 @@
+import { Button } from "@chakra-ui/react";
+
 export default function Survey() {
-  return <div>Survey Page</div>;
+  const speechMe = () => {
+    var msg = "Danny you lying piece of shit!";
+    const utterance = new SpeechSynthesisUtterance(msg);
+    speechSynthesis.speak(utterance);
+  };
+  return (
+    <div>
+      <Button onClick={speechMe}>Click me</Button>
+    </div>
+  );
 }
